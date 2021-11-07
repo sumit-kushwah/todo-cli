50ba0919c14c27e19705fffb5ffebb96677ceb42
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-08 00:07:36 +0530

    global delete and complete option added

--- cli.py
+++ cli.py
@@ -1,9 +1,14 @@
 import click
 from commands import Add, Update, Delete, List, Move, Reset, Show, ListAll
+from db import Database
 
-@click.group(help="This is your task manager")
-def cli():
-  pass
+@click.group(invoke_without_command=True, help="This is your task manager")
+@click.option('-d', '--delete', multiple=True)
+@click.option('-c', '--complete', multiple=True)
+def cli(delete, complete):
+  for id in delete:
+    db = Database()
+    db.deleteTask(id)
 
 cli.add_command(Add)
 cli.add_command(Delete)
