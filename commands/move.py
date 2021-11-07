7fc3c42c1b35a880740555d3de5d6480c0a9b98d
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-08 00:06:34 +0530

    fix object

--- commands/move.py
+++ commands/move.py
@@ -1,7 +1,9 @@
-import click, db
+import click
+from db import Database
 
 @click.command('move', help="Move task to a project.")
 @click.argument('id', type=int)
 @click.argument('project', type=click.STRING)
 def Move(id, project):
+  db = Database()
   db.changeTaskProject(id, project)
