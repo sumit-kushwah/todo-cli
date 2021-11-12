094ac7ba66d58dcdb80c9399c2156c1ac0d065d6
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-12 10:48:31 +0530

    fix help message

--- commands/update.py
+++ commands/update.py
@@ -3,7 +3,7 @@ from db import Database
 from task import Task
 from constants import DEFAULT_PROJECT, DATE_FORMATS, TIME_FORMATS, TODAY, RECUR_CHOICES
 
-@click.command('update', help="Add a new task.")
+@click.command('update', help="Update a existing task.")
 @click.option('-d', '--date', type=click.DateTime(formats=DATE_FORMATS))
 @click.option('-t', '--time', type=click.DateTime(formats=TIME_FORMATS))
 @click.option('-r', '--recur', type=click.INT)
