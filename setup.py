ae8421d7a62d08cc3d7f542ed5df13ee4cbfb342
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-13 15:53:30 +0530

    update version

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-from constants import ALIAS
 
 setup(
   name='todo-ksumit',
@@ -9,7 +8,7 @@ setup(
   This project provides simple task manager tool for developers in terminal.
   There are multiple commands supported. For docs and code please visit homepage url.
   """,
-  version='0.0.1',
+  version='0.0.2',
   py_modules=['todo-ksumit'],
   install_requires=[
     'Click',
@@ -17,7 +16,7 @@ setup(
   ],
   entry_points={
     'console_scripts': [
-      f'{ALIAS} = cli:cli',
+      't = cli:cli',
     ]
   }
 )
