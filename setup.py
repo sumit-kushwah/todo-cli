fbfb46c9ab258542ba8bb2b9b188edd0daee177b
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-13 15:30:55 +0530

    update

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 from constants import ALIAS
 
 setup(
-  name='todo',
+  name='todo-ksumit',
+  author='Sumit kushwah',
+  url='https://github.com/sumit-kushwah/todo-cli',
   version='0.0.1',
-  py_modules=['todo'],
+  py_modules=['todo-ksumit'],
   install_requires=[
     'Click',
     'tabulate'
