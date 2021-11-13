4fbf933db91ff9d7e48e3a0dc379aeaa5a35f85f
sumit Kushwah <sumitkushwah@sumits-MacBook-Air.local>
2021-11-13 15:43:41 +0530

    description added

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
   name='todo-ksumit',
   author='Sumit kushwah',
   url='https://github.com/sumit-kushwah/todo-cli',
+  description="""
+  This project provides simple task manager tool for developers in terminal.
+  There are multiple commands supported. For docs and code please visit homepage url.
+  """,
   version='0.0.1',
   py_modules=['todo-ksumit'],
   install_requires=[
